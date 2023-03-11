330d35d28d42ba2e877629acbe6c242e4175ddbf
nomanajaz65 <nomanajaz65@gmail.com>
2023-03-11 01:13:15 -0800

    done

---
+++ app.js
@@ -0,0 +1,16 @@
+var player1 = prompt('enter player1 name ')
+var player2 = prompt('enter player2 name ')
+
+var toss= Math.random()*2
+var change = Math.floor (toss)
+console.log(change)
+
+
+if
+(change == 0){
+    document.write( " head wins" )
+}
+
+else {
+    document.write( " tail wins")
+}
